dc989796f03568ccede72a4c64a06a64672c0e2b
Sean <seanzhu@Seans-Mac-mini.local>
2022-07-22 17:45:01 -0600

    update product image on index and product page

--- components/Product.jsx
+++ components/Product.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import Link from 'next/link'
 import { urlFor } from '../lib/client'
 
-const Product = ( {product: {image, name, slug, price}} ) => {
+const Product = ( {product: {iamge, name, slug, price}} ) => {
    
   return (
     <div>
       <Link href={`/product/${slug.current}`}>
         <div className="product-card">
           <img 
-            src={urlFor(image && image[0]).url}
+            src={urlFor(iamge && iamge[0])}
             width={250}
             height={250}
             className="product-image"
