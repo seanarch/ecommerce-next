532ff13e0dfe32212220b5dfe4bd9a7ed28dfad0
Sean <seanzhu@Seans-Mac-mini.local>
2022-07-22 16:48:01 -0600

    Update header, footer and products

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,36 @@
  import React from 'react'
- 
- const index = () => {
+ import { client } from '../lib/client'
+ import {Product, FooterBanner, HeroBanner} from '../components'
+
+ const index = ({products, bannerData}) => {
    return (
      <>
-       HeroBanner
-
+       <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+         
        <div className='products-heading'>
-          <h2>Beset Selling Products</h2>
+          <h2>Best Selling Products</h2>
           <p>Speakers of many variations</p>
        </div>
        <div className='products-container'>
-          {['Product 1', 'Product 2'].map(
-            (product)=> product)}
+          {products?.map(
+            (product)=> <Product key={product._id} product={product} />)}
        </div>
 
-       Footer
+       <FooterBanner footerBanner={bannerData && bannerData[0]} />
      </>
    )
+ }
+
+ export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: {products, bannerData}
+  }
  }
  
  export default index
